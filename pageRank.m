21d99ebd264cfda5d6a4ebe0f7477183f05ee955
violinbeats <vincentsu.violin@gmail.com>
2015-09-13 22:33:18 -0700

    updated comment

--- pageRank.m
+++ pageRank.m
@@ -1,22 +1,23 @@
-# PageRank algorithm with mechanism that deals with dead-ends and spider traps
-# M: adjacency matrix
-# r: rank
-# rSum: sum of your rank scores
-# beta: teleport probability parameter
-# epsilon: a range to check whether r has converged
+% PageRank algorithm with mechanism that deals with dead-ends and spider traps
+% M: adjacency matrix
+% r: rank
+% rSum: sum of your rank scores
+% beta: taxation probability parameter - %teleport
+% epsilon: a range to check whether r has converged
 
-M = [0, 0, 0; 0.5, 0, 0; 0.5, 1, 1];
+M = [0, 0, 1; 0.5, 0, 0; 0.5, 1, 0];
 N = rows(M);
-rSum = 3;
+rSum = 1;
 beta = 0.7;
 const = ones(N, 1) * ((1 - beta) / N);
 
-r = ones(N, 1) * 1 / N;
+r = ones(N, 1) / N;
 
 epsilon = 0.000000001;
 
 do
 	r_pre = r;
+	r = M * r;
 	r = beta * M * r + const;
 	s = sum(r);
 	if (s < rSum)
